bf2e3d8ceb51b8c86747fb1df22d98ce21524719
Faiz Farhan <mfaiz_farhan@hotmail.com>
2023-05-05 15:44:40 +0800

    Added timesheet feature to app

---
+++ app/Filament/Resources/TimesheetResource/Pages/CreateTimesheet.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Filament\Resources\TimesheetResource\Pages;
+
+use App\Filament\Resources\TimesheetResource;
+use Filament\Pages\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateTimesheet extends CreateRecord
+{
+    protected static string $resource = TimesheetResource::class;
+    
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        $data['user_id'] = auth()->id();
+        $data['status'] = 'Draft';
+        
+        return $data;
+    }
+
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
+}
